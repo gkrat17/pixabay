a2743897d69b3a807bf9a2f40caeeb6ba9d649bb
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2023-12-02 14:09:50 +0400

    Initial commit

---
+++ PixaBay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixaBay
+//
+//  Created by Giorgi Kratsashvili on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
